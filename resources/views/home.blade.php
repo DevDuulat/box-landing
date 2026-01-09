5a936e6dce5a9b6817226cd8eaebd9b3e674bc9c
DevDuulat <duulat.bekm@mail.ru>
2026-01-09 21:38:05 +0600

    fix(style): Remove overflow-y-auto from sidebar
    The `overflow-y-auto` class was causing issues with the custom scrollbar on the sticky sidebar. Removing it allows the `custom-scrollbar` class to manage overflow correctly.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="flex flex-col md:flex-row min-h-screen pt-20 md:pt-24 bg-white" x-data="productsComponent()">
 
         <aside class="hidden md:block w-64 lg:w-72 shrink-0 bg-[#111111]">
-            <div class="sticky top-24 h-[calc(100vh-6rem)] overflow-y-auto custom-scrollbar">
+            <div class="sticky top-24 h-[calc(100vh-6rem)] custom-scrollbar">
                 @include('sections.sidebar')
             </div>
         </aside>
